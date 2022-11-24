bdd60d6d25a78141339eb576ba0bd3724e2fa448
AndreCypriano23 <andrecypriano23@outlook.com>
2022-11-23 21:02:26 -0300

    Componente de botão desenvolvido e Event Binding

--- src/app/shared/componentes/yes-no-button-group/yes-no-button-group.component.ts
+++ src/app/shared/componentes/yes-no-button-group/yes-no-button-group.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-yes-no-button-group',
@@ -9,7 +9,8 @@ export class YesNoButtonGroupComponent implements OnInit {
   //@Input para dizer qual é o valor atual dele, yes, no, ou nada
   @Input() public value: string = null;
   @Input() public label = '';
-
+  @Output() public valueChange = new EventEmitter<string>();
+  public options = YesNoButtonGroupOptions;
 
   constructor() { }
 
@@ -18,9 +19,12 @@ export class YesNoButtonGroupComponent implements OnInit {
 
   public activate(value: string): void{
     this.value = value;
-
+    this.valueChange.emit(this.value);//Estou notificando com o mundo externo de que o valor mudou
   }
 
+}
 
-
+enum YesNoButtonGroupOptions{
+  YES = 'yes',
+  NO = 'no'
 }
